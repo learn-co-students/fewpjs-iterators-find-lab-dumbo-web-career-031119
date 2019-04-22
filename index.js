2b08493f4aac6d72a9de6a3054955de33b10e2ff
Jeff Yu <jwy5140@gmail.com>
2019-04-22 17:23:41 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  return (arr.find(r => r.result === "W") ? arr.find(r => r.result === "W").year : arr.find(r => r.result === "W"));
+}
