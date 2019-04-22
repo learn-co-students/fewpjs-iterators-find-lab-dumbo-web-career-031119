9ca34c5f7653201fc07d28d8039a37f8fef68244
github username <itsDario1@gmail.com>
2019-04-22 17:35:48 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(whash) {
+  let value = whash.find(s => s.result === 'W');
+  return !!value ? value.year : undefined;
+}
