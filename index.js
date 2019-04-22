c9d59dc82f52cbcea0abb4031904956c3cf88fc8
tpr1992 <terence.pataneronan@gmail.com>
2019-04-22 17:52:45 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// const superbowlWin = (arr) => {
+//   let win = arr.find(obj => {
+//     obj.result === "W"
+// // )
+//   }
+//   return !!obj ? obj.year : undefined
+// })
+// }
+
+const superbowlWin = (arr) => {
+let result = arr.find(obj => obj.result === "W" )
+// !!arr ? arr.year : undefined
+return !!result ? result.year : undefined
+}
